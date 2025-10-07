4229e2ea013168b8cd7e36c926c2672f48a512a1
Thanujan <110326955+daemonexe@users.noreply.github.com>
2025-10-06 23:03:54 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
     // vite.config.js
     import { defineConfig } from 'vite';
     import react from '@vitejs/plugin-react';
-    import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 
     export default defineConfig({
       plugins: [
